fc4d81d0caf433112eb4c0c305553bda801ff506
Anthony <anthony@mirahi.io>
2022-03-28 11:06:57 +0200

    :fire: remove nuxt from branch

--- nuxt-3/composables/useSync.ts
+++
@@ -1,9 +0,0 @@
-import { watch } from 'vue'
-
-export default <T extends object>(values: [T, T], setters: [Function, Function]) => {
-  const [first, second] = values
-  const [firstSetter, secondSetter] = setters
-
-  watch(first, (newVal) => firstSetter(newVal))
-  watch(second, (newVal) => secondSetter(newVal))
-}
